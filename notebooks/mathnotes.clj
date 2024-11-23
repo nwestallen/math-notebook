759ef99af1b7a35d634a366b24e8eaf594443299
Nick <nwestallen@gmail.com>
2024-11-22 20:33:20 -0800

    add table of contents

--- notebooks/mathnotes.clj
+++ notebooks/mathnotes.clj
@@ -1,19 +1,34 @@
-;; # Math Notes
+;;# Math Notes
 ^{:nextjournal.clerk/visibility {:code :hide}}
 (ns mathnotes
+  {:nextjournal.clerk/toc true}
   (:require [nextjournal.clerk :as clerk]))
 
-;; Ok hopefully I am able to do some notebook stuff
+;;## Trigonometry
 
-^{:nextjournal.clerk/visibility {:code :hide :result :hide}}
-(def equation (clerk/tex "
-  \\frac{d}{dt} \\frac{∂ L}{∂ \\dot{q}} - \\frac{∂ L}{∂ q} = 0
-"))
+;;#### Reciprocal Functions
+^{:nextjournal.clerk/visibility {:code :hide}}
+(clerk/table {:head ["Function", "Definition"]
+              :rows [[(clerk/tex "\\sec(x)"), (clerk/tex "\\frac{1}{\\cos(x)}")]
+                     [(clerk/tex "\\csc(x)"), (clerk/tex "\\frac{1}{\\sin(x)}")]
+                     [(clerk/tex "\\cot(x)"), (clerk/tex "\\frac{\\cos(x)}{\\sin(x)}")]]})
 
-;;**Trigonometric Derivatives**
+;;#### Pythagorean Identites
+^{:nextjournal.clerk/visibility {:code :hide}}
+(clerk/table {:rows [[(clerk/tex "\\sin^2(x) + \\cos^2(x) = 1")]
+                     [(clerk/tex "1 + \\cot^2(x) = \\csc^2(x)")]
+                     [(clerk/tex "1 + \\tan^2(x) = \\sec^2(x)")]
+                     [(clerk/tex "\\sec^2(x) + \\csc^2(x) = \\sec^2(x)\\csc^2(x)")]]})
+
+;;#### Trigonometric Derivatives
 ^{:nextjournal.clerk/visibility {:code :hide}}
 (clerk/table {:head [(clerk/tex "\\bf f(x)"), (clerk/tex "\\bf f'(x)")]
               :rows [[(clerk/tex "\\sin(x)"), (clerk/tex "\\cos(x)")]
                      [(clerk/tex "\\cos(x)"), (clerk/tex"-\\sin(x)")]
-                     [(clerk/tex "\\tan(x)"), (clerk/tex"\\sec^2(x)")]]})
+                     [(clerk/tex "\\tan(x)"), (clerk/tex"\\sec^2(x)")]
+                     [(clerk/tex "\\sec(x)"), (clerk/tex "\\sec(x)\\tan(x)")]
+                     [(clerk/tex "\\csc(x)"), (clerk/tex "-\\csc(x)\\cot(x)")]
+                     [(clerk/tex "\\cot(x)"), (clerk/tex "-\\csc^2(x)")]]})
+
+
 
